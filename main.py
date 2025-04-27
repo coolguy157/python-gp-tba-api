135829f7dc7ebbdde6cbdf8555c49f00890546ee
MatthewKrzewinski <m38802@student.ghctk12.com>
2025-04-26 23:16:28 -0500

    fixes script and allows for top x teams

--- main.py
+++ main.py
@@ -1,43 +1,45 @@
 import requests
 import os
+from dotenv import load_dotenv
+import bisect
 
-# Your API key for The Blue Alliance
-api_key = my_secret = os.environ['API_KEY']
+load_dotenv()
 
-# Function to fetch teams for the 2024 season from The Blue Alliance API
-def fetch_teams_for_2024():
-    teams = []
-    page_num = 0
-    while True:
-        response = requests.get(f'https://www.thebluealliance.com/api/v3/teams/{page_num}/keys',
-                                headers={'X-TBA-Auth-Key': api_key})
-        if response.status_code != 200 or not response.json():
-            break
-        teams.extend(response.json())
-        page_num += 1
-    return teams
+api_key = os.environ['TBA_API_KEY']
 
-# Fetch all teams for 2024
-teams_2024 = fetch_teams_for_2024()
+print("Enter the number of teams you want: ", end='')
+k = int(input())
+teams = [(-1, '')] # sorted list of (gp awards, [team keys])
 
-max_teams = []
-max_gp = -1
+page_num = 0
 
-for team in teams_2024:
-  gp_count = 0
-  response = requests.get(f'https://www.thebluealliance.com/api/v3/team/{team}/awards',
-    headers={'X-TBA-Auth-Key': api_key})
-  resp = response.json()
-  if (len(resp) < max_gp):
-    continue
-  for award in resp:
-    if award.get('award_type') == 11:
-      gp_count += 1
-  if gp_count > max_gp:
-    max_gp = gp_count
-    max_teams = [team]
-  elif gp_count == max_gp:
-    max_teams.append(team)
+while True:
+  response = requests.get(f'https://www.thebluealliance.com/api/v3/teams/{page_num}/keys', headers={'X-TBA-Auth-Key': api_key})
+  if response.status_code != 200 or not response.json():
+    break
+  for key in response.json():
+    response = requests.get(f'https://www.thebluealliance.com/api/v3/team/{key}/awards', headers={'X-TBA-Auth-Key': api_key})
+    resp = response.json()
+    if len(teams) < k: #insert
+      gp_count = sum(1 for aw in resp if aw.get('award_type') in (11, 19))
+      idx = bisect.bisect_left([(-c, '') for c, t in teams], (-gp_count, '')) #we always want an early insert
+      if (teams[idx][0] == gp_count): # we do a little appending
+        teams[idx][1].append(key)
+      else:
+        teams.insert(idx, (gp_count, [key]))
+      # print(teams)
+    else:
+      if (len(resp) < teams[-1][0]):
+        continue
+      gp_count = sum(1 for aw in resp if aw.get('award_type') in (11, 19))
+      if gp_count >= teams[-1][0]:
+        idx = bisect.bisect_left([(-c, '') for c, t in teams], (-gp_count, '')) #we always want an early insert
+        if (teams[idx][0] == gp_count): # we do a little appending
+          teams[idx][1].append(key)
+        else:
+          teams.insert(idx, (gp_count, [key]))
+          del teams[-1]
+        # print(teams)
+  page_num += 1
 
-print(max_teams, max_gp)
-  #GP is 11
+print(teams)
